7f1429e9beed78afb8660a3169be5e789e3a62f9
Yanis Colléaux <yaniscolleaux223@gmail.com>
2024-01-22 17:00:41 +0100

    ex6

--- src/TP2/main.py
+++ src/TP2/main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import pytz
 import logging
 import sys
+import os
 
 
 def get_fichier_sortie_args():
@@ -9,17 +10,25 @@ def get_fichier_sortie_args():
         return sys.argv[1]
     return "traitement.log"
 
+
 def get_fichier_sortie_env():
+    from dotenv import load_dotenv
+    load_dotenv()
+    if os.environ["CHEMIN_FICHIER_LOG"] is not None:
+        return os.environ["CHEMIN_FICHIER_LOG"]
     if len(sys.argv) > 1:
         return sys.argv[1]
     return "traitement.log"
 
+
 def get_fichier_sortie():
     return get_fichier_sortie_args()
 
+
 timezone = pytz.timezone('Europe/Paris')
 
-logging.basicConfig(filename="traitement.log", encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename="traitement.log", encoding='utf-8',
+                    level=logging.DEBUG)
 logging.info("Lancement du traitement")
 logging.debug(f"Demande d'heure sur le timezone : {timezone}")
 
