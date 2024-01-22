db4d6f52ff82ec4cc552cbbc2a78d1c278660334
Yanis Colléaux <yaniscolleaux223@gmail.com>
2024-01-22 14:47:48 +0100

    ex3

--- TP2/main.py
+++ TP2/main.py
@@ -1,5 +1,16 @@
 from datetime import datetime
 import pytz
+import logging
+
+timezone = pytz.timezone('Europe/Paris')
+
+logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
+logging.info(f"Lancement du traitement")
+logging.debug(f"Demande d'heure sur le timezone : {timezone}")
+
+if timezone is None:
+    logging.error("aucune timezone n'a été renseignée")
+    raise ValueError("aucune timezone n'a été renseignée")
 
 timezone_paris = pytz.timezone('Europe/Paris')
 timezone_reunion = pytz.timezone('Indian/Reunion')
