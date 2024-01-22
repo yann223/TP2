ffdec38db101e1001eb8a9de033707f6d88ef23c
Yanis Colléaux <yaniscolleaux223@gmail.com>
2024-01-22 15:20:31 +0100

    ex4 debut

--- TP2/main.py
+++ TP2/main.py
@@ -5,7 +5,7 @@ import logging
 timezone = pytz.timezone('Europe/Paris')
 
 logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
-logging.info(f"Lancement du traitement")
+logging.info("Lancement du traitement")
 logging.debug(f"Demande d'heure sur le timezone : {timezone}")
 
 if timezone is None:
@@ -15,9 +15,11 @@ if timezone is None:
 timezone_paris = pytz.timezone('Europe/Paris')
 timezone_reunion = pytz.timezone('Indian/Reunion')
 
+
 def get_date_formatted(timezone):
     date = datetime.now(timezone)
     return date.strftime("%H:%M:%S")
+
 
 print(get_date_formatted(timezone_paris))
 print(get_date_formatted(timezone_reunion))
